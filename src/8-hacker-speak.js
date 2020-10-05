e741a594000d9736ce5e1e9cbc0efbc2ee6e918e
emma-tech <emma.urman@gmail.com>
2020-10-05 17:50:02 +0200

    Test 8 passed

--- src/8-hacker-speak.js
+++ src/8-hacker-speak.js
@@ -1,3 +1,4 @@
+//PASS
 export const hackerSpeak = (words) => {
-  return words.replace(/S/gi,5);
+  return words.replace(/A/gi,4).replace(/E/gi,3).replace(/I/gi,1).replace(/O/gi,0).replace(/S/gi,5);
 }
